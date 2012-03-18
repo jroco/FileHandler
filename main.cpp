1e55693ba5fea8f9043410248ccb907889bf4a78
Administrador <Administrador@Minervas>
2012-03-18 18:09:48 -0400

    New log message.

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ using namespace std;
 int main(int argc, char** argv) {
 
     cout << "Program Begin" << endl;
-    cout << "Try to create the file C:\\file1.txt " << endl;
+    cout << "Try to create an input and an output file." << endl;
     
     ifstream file1in;
     ofstream file1out;
@@ -27,7 +27,7 @@ int main(int argc, char** argv) {
     int primero = 1, segundo = 2, tercero = 3;
     file1in >> primero >> segundo >> tercero;
     file1out << "La suma de los 3 primeros\n"
-            << "numeros de file1in.txt es: " << (primero + segundo + tercero) << "\n" << endl;
+                << "numeros de file1in.txt es: " << (primero + segundo + tercero) << "\n" << endl;
     
     file1in.close();
     file1out.close();
